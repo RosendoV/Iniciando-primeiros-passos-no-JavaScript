2621dfe5d24ff7c23d06025525a7246117cb51a2
VIVIAN MICAELY FIGUEIREDO ROSENDO <52223963803@imaccommand015.local>
2025-11-26 16:49:37 -0300

    iniciado os exercicios sobre o laco for

---
+++ Estrutura de Repetição/exercicio02.js
@@ -0,0 +1,12 @@
+//  Imprimir numeros de 1 a 10
+
+for(let i=2; i<=10; i = i + 2){
+    console.log("A volta que esta é:" + i)
+}
+
+// forma secundaria 
+// for (let i = 1; i <=10; i++){
+//     if (i % 2 == 0){
+//         console.log(i);
+//     }
+// }
---
+++ Estrutura de Repetição/for.js
@@ -0,0 +1,20 @@
+// sintaxe = escrita do código
+
+// for - laço de repetição
+// um laço de repetição tem voltas = loop
+
+// laço contade - sabemos quantas voltas 
+// laço condicional - ira fazer as voltas se a ccondicao for verdadeira
+
+// for([iniciação]; [condicao]; {incremento}){
+// }
+// a variavel I armazena as voltas
+// a condicao verifica o numero que o loop esta
+for (let i = 0; i <=9; i = i + 1){
+console.log("A volta que for está é:" = i);
+}
+
+//  For de um a dez
+for (let i = 1; i <=10; i = i + 1){
+console.log("A volta que for está é:" = i);
+}
